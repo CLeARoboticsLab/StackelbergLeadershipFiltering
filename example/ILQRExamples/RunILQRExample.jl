9c24590cf8b498ac9e06feae639d2182d06f51aa
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-03-05 21:56:49 -0600

    Minor changes

--- example/ILQRExamples/RunILQRExample.jl
+++ example/ILQRExamples/RunILQRExample.jl
@@ -1,9 +1,31 @@
 using Plots
 include("LQ_parameters.jl")
 
-selected_cost = quad_cost
+# Try out unicycle dynamics.
+dyn = UnicycleDynamics(num_players)
+xf = [5.; 5.; -pi/2; 0.]
 
-xs_i, us_i, is_converged, num_iters, costs = ilqr(T, t0, times, dyn, selected_cost, x0, us_1; max_iters=1000, step_size=1., threshold=1., verbose=true)
+# TODO(hmzh) - Implement this particular non-quadratic cost and test.
+# const_multiplier = 1.0
+# max_accel = 5.
+# max_omega = 1.
+# cost = ExampleILQRCost(pure_quad_cost, const_multiplier, max_accel, max_omega, xf, true)
+
+us_1 = zeros(udim(dyn), T)
+us_1[1,:] .= 0.1
+us_1[2,:] .= 0.01
+duration = (T-1) * dt
+us_1[1, :] .= (xf[3] - x0[3]) / duration # omega
+us_1[2, :] .= (xf[4] - x0[4]) / duration # accel
+
+# lin_dyn_0 = linearize_dynamics(dyn, (t0, t0+dt), x0, [zeros(udim(dyn))])
+# ctrl_strats, _ = solve_lqr_feedback(lin_dyn_0, quad_cost, T)
+# _, us_1 = unroll_feedback(dyn, times, ctrl_strats, x0)
+# us_1 = us_1[1] #+ randn(size(us_1[1]))
+
+selected_cost = quad_w_offset_cost
+
+xs_i, us_i, is_converged, num_iters, costs = ilqr(T, t0, times, dyn, selected_cost, x0, us_1; max_iters=1000, step_size=1., threshold=0.1, verbose=true)
 final_cost_total = evaluate(selected_cost, xs_i, [us_i])
 
 println("final: ", xs_i[:, T], " with trajectory cost: ", final_cost_total)
