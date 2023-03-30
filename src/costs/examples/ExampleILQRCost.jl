afadc507fc891edf4e348e25ab24c5b3f8bccadd
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-03-30 09:25:39 -0500

    Implement nonlinear cost example for SILQGames

--- src/costs/examples/ExampleILQRCost.jl
+++ src/costs/examples/ExampleILQRCost.jl
@@ -8,15 +8,17 @@ struct ExampleILQRCost <: NonQuadraticCost
     max_omega::Float64
     x_final::AbstractVector{<:Float64}
     is_nonlinear::Bool
+    player_idx::Int
 end
+ExampleILQRCost(quad_cost, c, max_accel, max_omega, x_final, is_nonlinear) = ExampleILQRCost(quad_cost, c, max_accel, max_omega, x_final, is_nonlinear, 1)
 
 # See slide 35 on Jake Levy's CLeAR Lab talk on 09/26/2022.
 function compute_cost(c::ExampleILQRCost, time_range, x::AbstractVector{Float64}, us::AbstractVector{<:AbstractVector{Float64}})
     # Start with the quadratic portions of the c.
     total = compute_cost(c.quad_cost, time_range, x, us)
 
-    accel = us[1][2]
-    omega = us[1][1]
+    accel = us[c.player_idx][2]
+    omega = us[c.player_idx][1]
     if c.is_nonlinear
         total += exp(c.c * (accel^2 - c.max_accel^2))
         total += exp(c.c * (omega^2 - c.max_omega^2))
@@ -30,14 +32,14 @@ function Gx(c::ExampleILQRCost, time_range, x::AbstractVector{Float64}, us::Abst
 end
 
 function Gus(c::ExampleILQRCost, time_range, x::AbstractVector{Float64}, us::AbstractVector{<:AbstractVector{Float64}})
-    accel = us[1][2]
-    omega = us[1][1]
+    accel = us[c.player_idx][2]
+    omega = us[c.player_idx][1]
     exp_term_alpha = exp(c.c * (accel^2 - c.max_accel^2))
     exp_term_omega = exp(c.c * (omega^2 - c.max_omega^2))
 
     out = Gus(c.quad_cost, time_range, x, us)
     if c.is_nonlinear
-        out[1] += 2 * c.c * [omega * exp_term_omega; accel * exp_term_alpha]'
+        out[c.player_idx] += 2 * c.c * [omega * exp_term_omega; accel * exp_term_alpha]'
     end
     return out
 end
@@ -47,14 +49,14 @@ function Gxx(c::ExampleILQRCost, time_range, x::AbstractVector{Float64}, us::Abs
 end
 
 function Guus(c::ExampleILQRCost, time_range, x::AbstractVector{Float64}, us::AbstractVector{<:AbstractVector{Float64}})
-    accel = us[1][2]
-    omega = us[1][1]
+    accel = us[c.player_idx][2]
+    omega = us[c.player_idx][1]
     exp_term_alpha = exp(c.c * (accel^2 - c.max_accel^2))
     exp_term_omega = exp(c.c * (omega^2 - c.max_omega^2))
 
     out = Guus(c.quad_cost, time_range, x, us)
     if c.is_nonlinear
-        out[1] += 2 * c.c * Diagonal([(1 + 2 * omega^2) * exp_term_omega, (1 + 2 * accel^2) * exp_term_alpha])
+        out[c.player_idx] += 2 * c.c * Diagonal([(1 + 2 * omega^2) * exp_term_omega, (1 + 2 * accel^2) * exp_term_alpha])
     end
     return out
 end
