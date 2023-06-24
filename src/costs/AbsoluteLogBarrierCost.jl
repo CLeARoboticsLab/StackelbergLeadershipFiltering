b6b3c4718f9a77e5290280d437eba067ef0efb7e
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-06-23 17:37:31 -0700

    Add some helpers that avoid errors.

--- src/costs/AbsoluteLogBarrierCost.jl
+++ src/costs/AbsoluteLogBarrierCost.jl
@@ -6,19 +6,37 @@ struct AbsoluteLogBarrierCost <: NonQuadraticCost
     is_lower_bound::Bool # true indicates lower bound barrier, false indicates upper bound
 end
 
+const LARGE_NUMBER = 1e6
+
 # Compute input to log function based on whether we have an upper or lower bound.
 function _get_input(c::AbsoluteLogBarrierCost, x)
     return (c.is_lower_bound) ? x[c.idx] - c.offset : c.offset - x[c.idx]
 end
 
+function _violates_bound(c::AbsoluteLogBarrierCost, x)
+    violates_lower_bound = c.is_lower_bound && x[c.idx] ≤ c.offset
+    violates_upper_bound = !c.is_lower_bound && x[c.idx] ≥ c.offset
+    return violates_upper_bound || violates_lower_bound
+end
+
 function compute_cost(c::AbsoluteLogBarrierCost, time_range, x::AbstractVector{Float64}, us::AbstractVector{<:AbstractVector{Float64}})
-    input = _get_input(c, x)
-    return -log(input)
+    if _violates_bound(c, x)
+        return LARGE_NUMBER
+    else
+        input = _get_input(c, x)
+        return -log(input)
+    end
 end
 
 # Derivative term c(x_i - a_i)^-1 at {c.idx}th index.
 function Gx(c::AbsoluteLogBarrierCost, time_range, x::AbstractVector{Float64}, us::AbstractVector{<:AbstractVector{Float64}})
     out = zeros(1, size(x, 1))
+
+    # If we are in the wrong place, choose a gradient of 0 and hope constraints pull it back out.
+    if _violates_bound(c, x)
+        return out
+    end
+
     out[1, c.idx] = -(x[c.idx] - c.offset)^(-1)
     return out
 end
@@ -31,6 +49,12 @@ end
 # diagonal matrix, with diagonals either 0 or c(x_i - a_i)^-2, no cross terms
 function Gxx(c::AbsoluteLogBarrierCost, time_range, x::AbstractVector{Float64}, us::AbstractVector{<:AbstractVector{Float64}})
     out = zeros(size(x, 1), size(x, 1))
+
+    # If we are in the wrong place, choose a gradient of 0 and hope constraints pull it back out.
+    if _violates_bound(c, x)
+        return out
+    end
+
     # Set the hessian at one entry since the cost is independent of other costs.
     out[c.idx, c.idx] = (x[c.idx] - c.offset)^(-2)
     return out
