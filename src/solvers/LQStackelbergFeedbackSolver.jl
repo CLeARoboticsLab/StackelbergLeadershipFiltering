ea8e4ade683fe9eb0da263d24115f58253cdb1c3
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-07-12 20:11:17 -0500

    error message changes

--- src/solvers/LQStackelbergFeedbackSolver.jl
+++ src/solvers/LQStackelbergFeedbackSolver.jl
@@ -72,10 +72,10 @@ function solve_lq_stackelberg_feedback(dyns::AbstractVector{LinearDynamics},
         R₂₁ = get_homogenized_control_cost_matrix(costs[follower_idx], leader_idx)
 
         # Ensure that we have (semi-)positive definiteness in the relevant costs.
-        @assert isposdef(Q_leader) || minimum(eigvals(Q_leader)) == 0
-        @assert isposdef(Q_follower) || minimum(eigvals(Q_follower)) == 0
-        @assert isposdef(R₁₁)
-        @assert isposdef(R₂₂)
+        @assert isposdef(Q_leader) || minimum(eigvals(Q_leader)) == 0 string("Q leader not semi-posdef, min eigvals: ", minimum(eigvals(Q_leader)))
+        @assert isposdef(Q_follower) || minimum(eigvals(Q_follower)) == 0 string("Q follower not semi-posdef, min eigvals: ", minimum(eigvals(Q_follower)))
+        @assert isposdef(R₁₁) string("R₁₁ not posdef, min eigvals: ", minimum(eigvals(R₁₁)))
+        @assert isposdef(R₂₂) string("R₂₂ not posdef, min eigvals: ", minimum(eigvals(R₂₂)))
 
         Lₖ₊₁ = [all_Ls[leader_idx][:, :, tt+1], all_Ls[follower_idx][:, :, tt+1]]
 
