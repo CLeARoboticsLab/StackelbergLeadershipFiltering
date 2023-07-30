b9f71072023c76ec55e821d8bef8a83e31bed0df
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-07-30 14:28:02 -0500

    plot improvements, SILQGames optimizations

--- example/LeadershipFilteringExamples/GenerateLQLeaderFilterAnimation.jl
+++ example/LeadershipFilteringExamples/GenerateLQLeaderFilterAnimation.jl
@@ -67,7 +67,7 @@ for t in iter1
 
     p1b = plot_leadership_filter_measurement_details(num_particles, sg_objs[t], true_xs[:, 1:T], x̂s)
 
-    p5, p6 = make_probability_plots(times[1:T], t, probs[1:T]; include_gt_val=leader_idx)
+    p5, p6 = make_probability_plots(times[1:T], probs[1:T]; t_idx=t, include_gt=leader_idx)
     plot!(p5, title="")
     plot!(p6, title="")
 
@@ -88,11 +88,11 @@ end
 # This plot need not be in the loop.
 title="x-y plot of agent positions over time"
 p1a = plot_leadership_filter_positions(sg_objs[1].dyn, true_xs[:, 1:T], x̂s[:, 1:T], zs[:, 1:T])
-plot!(p1a, title=title, legend=:outertopright)
+plot!(p1a, title=title)
 
 iter = ProgressBar(2:T)
 anim = @animate for t in iter
-    p = @layout [a b; grid(1, 3); e f]
+    p = @layout [a{1.0h} b{1.0h}; grid(1, 3); e f]
 
     plot_title = string("LF (", t, "/", T, ") on Stack(L=P", leader_idx, "), Ts=", Ts, ", Ns=", num_particles, ", p(transition)=", p_transition, ", #games: ", num_games)
     p1b = plot_leadership_filter_measurement_details(num_particles, sg_objs[t], true_xs[:, 1:T], x̂s)
@@ -123,7 +123,7 @@ anim = @animate for t in iter
     # probability plots 5 and 6
     title5 = "Probability over time for P1"
     title6 = "Probability over time for P2"
-    p5, p6 = make_probability_plots(times[1:T], t, probs[1:T]; include_gt_val=leader_idx)
+    p5, p6 = make_probability_plots(times[1:T], probs[1:T]; t_idx=t, include_gt=leader_idx)
     plot!(p5, title=title5)
     plot!(p6, title=title6)
 
--- src/PlotUtils.jl
+++ src/PlotUtils.jl
@@ -5,6 +5,11 @@
 using LaTeXStrings
 using Plots
 
+function get_standard_plot(include_legend=:outertop)
+    return plot(legendfontsize=6, tickfontsize=16, fontsize=12, labelfontsize=14, legend=include_legend, legend_column=-1, ncol=2, fg_legend = :transparent)
+end
+export get_standard_plot
+
 # By default rotates clockwise 90 degrees
 function rotate_state(dyn::UnicycleDynamics, xs)
     x1_idx = xidx(dyn, 1)
@@ -23,7 +28,7 @@ end
 export rotate_state
 
 # TODO(hamzah) - refactor this to be tied DoubleIntegrator Dynamics instead of Linear Dynamics.
-function plot_states_and_controls(dyn::LinearDynamics, times, xs, us)
+function plot_states_and_controls(dyn::LinearDynamics, times, xs, us; include_legend=:none)
     @assert num_agents(dyn) == 2
     @assert xdim(dyn) == 8
     @assert udim(dyn, 1) == 2
@@ -36,7 +41,8 @@ function plot_states_and_controls(dyn::LinearDynamics, times, xs, us)
     y2_idx = yidx(dyn, 2)
 
     title1 = "pos. traj."
-    q1 = plot(legend=:outertopright, title=title1, xlabel=L"$x$ (m)", ylabel=L"$y$ (m)")
+    q1 = get_standard_plot(include_legend)
+    plot!(title=title1, xlabel=L"$x$ (m)", ylabel=L"$y$ (m)")
     plot!(q1, xs[x1_idx, :], xs[y1_idx,:], label="P1 pos", color=:red)
     plot!(q1, xs[x2_idx,:], xs[y2_idx, :], label="P2 pos", color=:blue)
 
@@ -44,32 +50,38 @@ function plot_states_and_controls(dyn::LinearDynamics, times, xs, us)
     q1 = scatter!([x₁[x2_idx]], [x₁[y2_idx]], color=:blue, label="P2 start")
 
     title2a = "x-pos"
-    q2a = plot(legend=:outertopright, title=title2a, xlabel="t (s)", ylabel="x (m)")
+    q2a = get_standard_plot(include_legend)
+    plot!(title=title2a, xlabel="t (s)", ylabel="x (m)")
     plot!(times, xs[x1_idx,:], label="P1 px")
     plot!(times, xs[x2_idx,:], label="P2 px")
 
     title2b = "y-pos"
-    q2b = plot(legend=:outertopright, title=title2b, xlabel="t (s)", ylabel="y (m)")
+    q2b = get_standard_plot(include_legend)
+    plot!(title=title2b, xlabel="t (s)", ylabel="y (m)")
     plot!(times, xs[y1_idx,:], label="P1 py")
     plot!(times, xs[y2_idx,:], label="P2 py")
 
     title3 = "x-vel"
-    q3 = plot(legend=:outertopright, title=title3, xlabel="t (s)", ylabel="vel. (mps)")
+    q3 = get_standard_plot(include_legend)
+    plot!(title=title3, xlabel="t (s)", ylabel="vel. (mps)")
     plot!(times, xs[2,:], label="P1 vx")
     plot!(times, xs[6,:], label="P2 vx")
 
     title4 = "y-vel"
-    q4 = plot(legend=:outertopright, title=title4, xlabel="t (s)", ylabel="vel. (mps)")
+    q4 = get_standard_plot(include_legend)
+    plot!(title=title4, xlabel="t (s)", ylabel="vel. (mps)")
     plot!(times, xs[4,:], label="P1 vy")
     plot!(times, xs[8,:], label="P2 vy")
 
     title5 = "x-accel"
-    q5 = plot(legend=:outertopright, title=title5, xlabel="t (s)", ylabel="accel (mpss)")
+    q5 = get_standard_plot(include_legend)
+    plot!(title=title5, xlabel="t (s)", ylabel="accel (mpss)")
     plot!(times, us[1][1, :], label="P1 ax")
     plot!(times, us[2][1, :], label="P2 ax")
 
     title6 = "y-accel"
-    q6 = plot(legend=:outertopright, title=title6, xlabel="t (s)", ylabel="accel (mpss)")
+    q6 = get_standard_plot(include_legend)
+    plot!(title=title6, xlabel="t (s)", ylabel="accel (mpss)")
     plot!(times, us[1][2, :], label="P1 ay")
     plot!(times, us[2][2, :], label="P2 ay")
 
@@ -77,7 +89,7 @@ function plot_states_and_controls(dyn::LinearDynamics, times, xs, us)
 end
 
 # TODO(hamzah) - refactor this to adjust based on number of players instead of assuming 2.
-function plot_states_and_controls(dyn::UnicycleDynamics, times, xs, us)
+function plot_states_and_controls(dyn::UnicycleDynamics, times, xs, us; include_legend=:none)
     @assert num_agents(dyn) == 2
 
     x₁ = xs[:, 1]
@@ -88,7 +100,8 @@ function plot_states_and_controls(dyn::UnicycleDynamics, times, xs, us)
     y2_idx = yidx(dyn, 2)
 
     title1 = "pos. traj."
-    q1 = plot(legend=:outertopright, title=title1, xlabel="x (m)", ylabel="y (m)")
+    q1 = get_standard_plot(include_legend)
+    plot!(title=title1, xlabel="x (m)", ylabel="y (m)")
     plot!(q1, xs[x1_idx, :], xs[y1_idx, :], label="P1 pos", color=:red)
     plot!(q1, xs[x2_idx,:], xs[y2_idx, :], label="P2 pos", color=:blue)
 
@@ -96,32 +109,38 @@ function plot_states_and_controls(dyn::UnicycleDynamics, times, xs, us)
     q1 = scatter!([x₁[x2_idx]], [x₁[y2_idx]], color=:blue, label="start P2")
 
     title2a = "x-pos"
-    q2a = plot(legend=:outertopright, title=title2a, xlabel="t (s)", ylabel="x (m)")
+    q2a = get_standard_plot(include_legend)
+    plot!(title=title2a, xlabel="t (s)", ylabel="x (m)")
     plot!(times, xs[x1_idx,:], label="P1 px")
     plot!(times, xs[x2_idx,:], label="P2 px")
 
     title2b = "y-pos"
-    q2b = plot(legend=:outertopright, title=title2b, xlabel="t (s)", ylabel="y (m)")
+    q2b = get_standard_plot(include_legend)
+    plot!(title=title2b, xlabel="t (s)", ylabel="y (m)")
     plot!(times, xs[y1_idx,:], label="P1 py")
     plot!(times, xs[y2_idx,:], label="P2 py")
 
     title3 = "θ"
-    q3 = plot(legend=:outertopright, title=title3, xlabel="t (s)", ylabel="θ (rad)")
+    q3 = get_standard_plot(include_legend)
+    plot!(title=title3, xlabel="t (s)", ylabel="θ (rad)")
     plot!(times, wrap_angle.(xs[3,:]), label="P1 θ")
     plot!(times, wrap_angle.(xs[7,:]), label="P2 θ")
 
     title4 = "vel"
-    q4 = plot(legend=:outertopright, title=title4, xlabel="t (s)", ylabel="vel. (mps)")
+    q4 = get_standard_plot(include_legend)
+    plot!(title=title4, xlabel="t (s)", ylabel="vel. (mps)")
     plot!(times, xs[4,:], label="P1 v")
     plot!(times, xs[8,:], label="P2 v")
 
     title5 = "ang vel"
-    q5 = plot(legend=:outertopright, title=title5, xlabel="t (s)", ylabel="ang. vel. (rad/s)")
+    q5 = get_standard_plot(include_legend)
+    plot!(title=title5, xlabel="t (s)", ylabel="ang. vel. (rad/s)")
     plot!(times, us[1][1, :], label="P1 ω")
     plot!(times, us[2][1, :], label="P2 ω")
 
     title6 = "accel"
-    q6 = plot(legend=:outertopright, title=title6, xlabel="t (s)", ylabel="accel (mpss)")
+    q6 = get_standard_plot(include_legend)
+    plot!(title=title6, xlabel="t (s)", ylabel="accel (mpss)")
     plot!(times, us[1][2, :], label="P1 accel")
     plot!(times, us[2][2, :], label="P2 accel")
 
@@ -135,7 +154,8 @@ function plot_convergence_and_costs(num_iters, threshold, conv_metrics, evaluate
     # Plot convergence metric max absolute state difference between iterations.
     conv_x = cumsum(ones(num_iters)) .- 1
     title8 = "convergence"
-    q8 = plot(title=title8, yaxis=:log, xlabel="# Iterations", ylabel="Max Absolute State Difference")
+    q8 = get_standard_plot()
+    plot!(title=title8, yaxis=:log, xlabel="# Iterations", ylabel="Max Absolute State Difference")
 
     conv_sum = conv_metrics[1, 1:num_iters] #+ conv_metrics[2, 1:num_iters]
     plot!(q8, conv_x, conv_sum, label="Merit Fn", color=:green)
@@ -157,7 +177,8 @@ function plot_convergence_and_costs(num_iters, threshold, conv_metrics, evaluate
     end
 
     title9 = "evaluated costs"
-    q9 = plot(title=title9, yaxis=:log, xlabel="# Iterations", ylabel="Cost")
+    q9 = get_standard_plot()
+    plot!(title=title9, yaxis=:log, xlabel="# Iterations", ylabel="Cost")
     plot!(conv_x, costs_1[1:num_iters], label="P1", color=:red)
     plot!(conv_x, costs_2[1:num_iters], label="P2", color=:blue)
 
@@ -180,7 +201,8 @@ function plot_leadership_filter_positions(dyn::Dynamics, true_xs, est_xs, zs)
     x2_idx = xidx(dyn, 2)
     y2_idx = yidx(dyn, 2)
 
-    p1 = plot(ylabel=L"$y$ m", xlabel=L"$x$ m")
+    p1 = get_standard_plot()
+    plot!(ylabel=L"$y$ m", xlabel=L"$x$ m", ylimit=(-2.5, 2.5), xlimit=(-2.5, 2.5))
     plot!(p1, true_xs[x1_idx, :], true_xs[y1_idx, :], label="True P1", color=:red, linewidth=2, ls=:dash)
     plot!(p1, true_xs[x2_idx, :], true_xs[y2_idx, :], label="True P2", color=:blue, linewidth=2, ls=:dash)
 
@@ -211,7 +233,8 @@ function plot_leadership_filter_measurement_details(num_particles, sg_t, true_xs
     x2_idx = xidx(sg_t.dyn, 2)
     y2_idx = yidx(sg_t.dyn, 2)
 
-    p2 = plot(ylabel=L"$y$ m", xlabel=L"$x$ m", ylimit=(-2.5, 2.5), xlimit=(-2.5, 2.5))
+    p2 = get_standard_plot()
+    plot!(ylabel=L"$y$ m", xlabel=L"$x$ m", ylimit=(-2.5, 2.5), xlimit=(-2.5, 2.5))
     plot!(p2, true_xs[x1_idx, :], true_xs[y1_idx, :], label="True P1", color=:black, linewidth=3)
     plot!(p2, true_xs[x2_idx, :], true_xs[y2_idx, :], label="True P2", color=:black, linewidth=3)
 
@@ -230,7 +253,7 @@ function plot_leadership_filter_measurement_details(num_particles, sg_t, true_xs
         # println("num iters 1, 2: ", sg_t.num_iterations, " ", sg_t.num_iterations[n])
         # println("num iters 1, 2: ", sg_t.num_iterations, " ", sg_t.num_iterations[n])
 
-        xks = transform_particle_fn(sg_t.xks[n, num_iter, :, :])
+        xks = transform_particle_fn(sg_t.xks[n, :, :])
 
         # TODO(hamzah) - change color based on which agent is leader
         color = (sg_t.leader_idxs[n] == 1) ? "red" : "blue"
@@ -247,25 +270,36 @@ export plot_leadership_filter_measurement_details
 
 # This function generates two probability plots (both lines on one plot is too much to see), one for the probablity of
 # each agent as leader.
-function make_probability_plots(times, t_idx, probs; include_gt=nothing)
-    t = times[t_idx]
+function make_probability_plots(times, probs; t_idx=nothing, include_gt=nothing, stddevs=nothing)
     T = length(times)
+    lower_p1, upper_p1 = (isnothing(stddevs)) ? (zeros(T), zeros(T)) : stddevs
 
     # probability plot for P1 - plot 5
-    p5 = plot(xlabel="t (s)", ylabel=L"""$\mathbb{P}(L=\mathcal{A}_1)$""", ylimit=(-0.1, 1.1), label="")
-    plot!(p5, times, probs, color=:red, label="P1")
+    p5 = get_standard_plot()
+    plot!(xlabel="t (s)", ylabel=L"""$\mathbb{P}(H_t=\mathcal{A}_1)$""", ylimit=(-0.1, 1.1), label="")
     if !isnothing(include_gt)
         plot!(p5, times, (include_gt%2) * ones(T), label="Truth", color=:green, linestyle=:dash, linewidth=2)
     end
-    plot!(p5, [t, t], [-0.05, 1.05], label="t=$(round.(t, digits=3)) s", color=:black, linestyle=:dot, linewidth=3)
+    if !isnothing(t_idx)
+        t = times[t_idx]
+        plot!(p5, [t, t], [-0.05, 1.05], label="t=$(round.(t, digits=3)) s", color=:black, linestyle=:dot, linewidth=3)
+    end
+
+    # Bound the stddevs to avoid going above 1 or below 0.
+    plot!(p5, times, probs, color=:red, label="P1", ribbon=(lower_p1, upper_p1), fillalpha=0.3)
 
      # probability plot for P2 - plot 6
-    p6 = plot(xlabel="t (s)", ylabel=L"""$\mathbb{P}(L=\mathcal{A}_2)$""", ylimit=(-0.1, 1.1), label="")
-    plot!(p6, times, 1 .- probs, color=:blue, label="P2")
+    p6 = get_standard_plot()
+    plot!(xlabel="t (s)", ylabel=L"""$\mathbb{P}(H_t=\mathcal{A}_2)$""", ylimit=(-0.1, 1.1), label="")
     if !isnothing(include_gt)
         plot!(p6, times, ((include_gt+1)%2) * ones(T), label="Truth", color=:green, linestyle=:dash, linewidth=2)
     end
-    plot!(p6, [t, t], [-0.05, 1.05], label="t=$(round.(t, digits=3)) s", color=:black, linestyle=:dot, linewidth=3)
+    if !isnothing(t_idx)
+        t = times[t_idx]
+        plot!(p6, [t, t], [-0.05, 1.05], label="t=$(round.(t, digits=3)) s", color=:black, linestyle=:dot, linewidth=3)
+    end
+    lower_p2, upper_p2 = upper_p1, lower_p1
+    plot!(p6, times, 1 .- probs, color=:blue, label="P2", ribbon=(lower_p2, upper_p2), fillalpha=0.3)
 
     return p5, p6
 end
