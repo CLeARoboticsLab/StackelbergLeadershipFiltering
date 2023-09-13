af9af6f30ee0256eeef3304c7019c39841e043fd
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-09-13 01:19:00 -0500

    Introduce elapsed times per iteration for SILQGames and per time step for LF.

--- example/SILQGamesExamples/RunMCSimulationsForLQGame.jl
+++ example/SILQGamesExamples/RunMCSimulationsForLQGame.jl
@@ -27,7 +27,7 @@ costs = [QuadraticCostWithOffset(ss_costs[1]), QuadraticCostWithOffset(ss_costs[
 # For player cost
 # costs = [pc_cost_1, pc_cost_2]
 
-num_sims = 20
+num_sims = 100
 
 data_folder = "mc_data"
 
