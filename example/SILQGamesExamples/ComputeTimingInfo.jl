72415a37389738ef0b96d6ad9ddc16d6407bf7e4
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-09-14 11:24:14 -0500

    bug 12

--- example/SILQGamesExamples/ComputeTimingInfo.jl
+++ example/SILQGamesExamples/ComputeTimingInfo.jl
@@ -50,6 +50,7 @@ function compute_leadership_filter_timing_info(data_folder, lf_filename)
     timings = lf_data["all_lf_iter_timings"]
     @assert lf_data["silq"]["T"] == size(timings, 2) "length 1: $(lf_data["silq_data"]["T"]), length 2: $(size(timings, 2))"
     @assert !any(iszero.(timings))
+    println(size(timings))
 
     mean_times_per_step = mean.(timings; dims=[1])
     std_times_per_step = std.(timings; dims=[1])
