1a0d5b8f683be8a5e19b1ef93cad15c25160686f
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-09-15 15:54:29 -0500

    change file

--- example/SILQGamesExamples/MCFileLocations.jl
+++ example/SILQGamesExamples/MCFileLocations.jl
@@ -19,7 +19,7 @@ function get_final_lq_paths_p1()
 
     # data_folder = ""
 
-    data_folder = "lq_mc100_L1_9_13_4_22"
+    data_folder = "lq_mc100_L1_9_13_22_32"
     lf_data_file = "lq_lf_mc100_L1_th0.015_ss0.01_M50.jld"
     silq_data_file = "lq_silq_mc100_L1_th0.015_ss0.01_M50.jld"
 
