8894d0b887696878287d04a882beebae4fba7204
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-09-21 12:30:30 -0500

    Add insets 9

--- src/PlotUtils.jl
+++ src/PlotUtils.jl
@@ -417,7 +417,7 @@ function plot_leadership_filter_measurement_details(dyn::Dynamics, particle_lead
             end
             scatter!(subplot=subplot_idx, [true_xs[x_idx, t]], [true_xs[y_idx, t]], color=:black, ms=10)
             # scatter!(subplot=subplot_idx, [est_xs[x_idx, t]], [est_xs[y_idx, t]], color=:turquoise2, ms=10)
-            scatter!(subplot=subplot_idx, [meas_xs[x_idx, t]], [meas_xs[y_idx, t]], markerstyle=:plus, color=:black, ms = 10)
+            scatter!(subplot=subplot_idx, [meas_xs[x_idx, t]], [meas_xs[y_idx, t]], marker=:plus, color=:black, ms = 10)
 
             # PLAYER 2 INSET
             subplot_idx = 3
@@ -471,7 +471,7 @@ function plot_leadership_filter_measurement_details(dyn::Dynamics, particle_lead
             end
             scatter!(subplot=subplot_idx, [true_xs[x_idx, t]], [true_xs[y_idx, t]], color=:black, ms=10)
             # scatter!(subplot=subplot_idx, [est_xs[x_idx, t]], [est_xs[y_idx, t]], color=:turquoise2, ms=10)
-            scatter!(subplot=subplot_idx, [meas_xs[x_idx, t]], [meas_xs[y_idx, t]], markerstyle=:plus, color=:black, ms=10)
+            scatter!(subplot=subplot_idx, [meas_xs[x_idx, t]], [meas_xs[y_idx, t]], marker=:plus, color=:black, ms=10)
         end
     end
 
